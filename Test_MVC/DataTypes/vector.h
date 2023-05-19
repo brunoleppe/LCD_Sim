7dda6bb4b595057ca12fd2e8cdcbe6e740673252
brunoleppe <bruno.leppe.dev@gmail.com>
2023-05-18 20:17:28 -0500

    New bru::vector class (minimal vector implementation).

---
+++ Test_MVC/DataTypes/vector.h
@@ -0,0 +1,22 @@
+//
+// Created by bruno on 18/05/23.
+//
+
+#ifndef LCDTEST_VECTOR_H
+#define LCDTEST_VECTOR_H
+
+namespace bru {
+
+template <typename T>
+class vector {
+private:
+    T* buffer;
+    int capacity = 32;
+    int size;
+public:
+
+};
+
+} // bru
+
+#endif //LCDTEST_VECTOR_H
