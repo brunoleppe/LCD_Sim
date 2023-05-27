67c3d101ef70aba527c62962c8330b8746b2d55a
bruno <bruno.leppe.dev@gmail.com>
2023-05-27 17:37:19 -0500

    Added accessibility for C

--- debug_bsp.h
+++ debug_bsp.h
@@ -9,7 +9,15 @@
 #define DEBUG_PRINT(...)     debug_printf(__VA_ARGS__)
 #define ERROR_PRINT(...)     error_printf(__VA_ARGS__)
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 void debug_printf(const char *fmt, ...);
 void error_printf(const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //READER_DEBUG_BSP_H
